095c4341d2f8cd1bc172f2243121bb1577b24047
g.driano <driano.gael@gmail.com>
2025-10-08 14:24:59 +0200

    fix/ passage de la rubrique data qrcode en base64

--- qr_code_service/models.py
+++ qr_code_service/models.py
@@ -3,8 +3,9 @@ from api.models import Ticket
 
 
 class QrCode(models.Model):
-    data = models.BinaryField(help_text="Données encodées dans le QR code")
+    data = models.TextField(help_text="Données encodées dans le QR code")
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='qrcodes', help_text="Ticket associé à ce QR code")
+    create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"QR Code {self.id} - {self.data}"
